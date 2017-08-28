61e8a551bcfec19a6e3b10613aa5520ece35f49d
Lykke-AndersenLab <t2shaw@ucsd.edu>
2017-08-27 18:08:40 -0700

    Began work on hamming filter

--- hammerTime.py
+++ hammerTime.py
@@ -1,21 +1,84 @@
-import distance, time
+import gzip, distance, time
+
+
+def reverseComplement(read):
+    """Returns the reverse complement of a single read"""
+
+    comp=[]
+
+    for j in read:
+        if j == 'A': comp.append('T')
+        elif j == 'T': comp.append('A')
+        elif j == 'C': comp.append('G')
+        elif j == 'G': comp.append('C')
+        else: comp.append('N')
+    comp.reverse()
+    comp=''.join(comp)
+    return(comp)
+def fastqParser(fileLoc, revComp = True):
+    """Reads in a fastq formatted file and returns a list of list of reads
+    Reads are assumed to be represented as 4 lines, the second line of which is the basecalls"""
+    if fileLoc[-3:] == ".gz":
+        fastq = gzip.open(fileLoc, 'rt').readlines()
+    else:
+        fastq = open(fileLoc, 'r').readlines()
+    reads =[]
+
+
+    for i in range(len(fastq)):
+            if revComp:
+                if (i - 1) % 4 == 0:
+                    reads.append(reverseComplement(fastq[i].rstrip()))
+            else:
+                if (i - 1) % 4 == 0:
+                    reads.append(fastq[i].rstrip())
+    if not reads[-1]: reads = reads[:-1]
+    return reads
+def uniqueFilter(readList, barcodeLength, barcodeMismatch =0):
+    barcoded = []
+    for read in readList:
+        barcoded.append([read[:-barcodeLength],read[-barcodeLength:]])
+    barcoded = sorted(barcoded, key = lambda x:x[0])
+    counts = []
+    tempBarcodes = []
+    currRead = barcoded[0][0]
+    for i in range(len(barcoded)):
+        if barcoded[i][0] == currRead:
+            tempBarcodes.append(barcoded[i][1])
+        else:
+            if not barcodeMismatch:
+                counts.append([currRead, len(set(tempBarcodes))])
+            else:
+                counts.append([currRead, uniqueNumber(tempBarcodes,mismatch=barcodeMismatch)])
+                currRead = barcoded[i][0]
+                tempBarcodes = [barcoded[i][1],]
+    counts.append([currRead, uniqueNumber(tempBarcodes, mismatch=barcodeMismatch)])
+    counts = sorted(counts, key = lambda x:x[1], reverse=True)
+    return counts
+def uniqueNumber(list, mismatch = 0):
+    unique = [list[0]]
+    for barcode in list:
+        flag = False
+        for item in unique:
+            if distance.hamming(barcode, item) <= mismatch:
+                flag = True
+                break
+        if not flag:
+            unique.append(barcode)
+    return len(unique)
 
 
 
 
-n = 1000000
-list = []
-for i in range(n):
-    list.append("abcdefghijk")
-print ("list made")
-start_time = time.time()
-
-count = 0
-for item in list:
-    distance.hamming('abcdefghijl', item)
-    count +=1
-    if count%100000==0: print(count/n)
 
 
 
-print("--- %s seconds ---" % (time.time() - start_time))
+
+
+start_time =time.time()
+reads = fastqParser("siLuc1_S1_L001_R1_001.fastq")
+print('readin successful')
+reads = uniqueFilter(reads, 12)
+print(reads[:20])
+print(time.time()-start_time)
+
